d16b1a7f5d595d3d72a32521089ce56a7187fa85
Jacob Adelgren <jadelgre@mymail.mines.edu>
2016-05-06 17:22:41 -0600

    remove FAB from adobe activity

--- app/src/main/java/howhighami/com/howhighami/AdobeEditor.java
+++ app/src/main/java/howhighami/com/howhighami/AdobeEditor.java
@@ -47,14 +47,6 @@ public class AdobeEditor extends AppCompatActivity {
         /* 3) Start the Image Editor with request code 1 */
         startActivityForResult(imageEditorIntent, 1);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     /* Handle the results */
